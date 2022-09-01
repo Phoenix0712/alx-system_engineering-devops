b4118ea9d378b375676b68517221a154a0c4a4e3
Gezai Enya <gezaienya@gmail.com>
2022-09-01 05:55:20 -0700

    script

--- 0x00-shell_basics/0-current_working_directory.sh
+++ 0x00-shell_basics/0-current_working_directory.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-echo "/root/alx-system_engineering-devops/0x00-shell_basics"
+path=$(realpath 0-current_working_directory.sh)
+DIR_PATH=$(dirname $path)
+echo $DIR_PATH
+
+#me="$(readlink -f ${BASH_SOURCE:-$0})"
+#PATH=$PATH:${me%/*}
+#DIR_PATH=$DIR_PATH:${me%/*}
+#echo $DIR_PATH
+
+
+# DIR_PATH=$(dirname $path)
+#echo ‘The directory path is’ $DPATH
+
 
